214c4398e9740d293910e3d471217dfb5d854df1
Mapache <roderickroco@gmail.com>
2020-06-03 14:04:50 -0400

    respaldo realizado dia 03 mes 06 año 2020

--- AngularJS/pizarra/src/app/app.module.ts
+++ AngularJS/pizarra/src/app/app.module.ts
@@ -3,27 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//Agregados 
 
-
-// Fin agregados
-
-//Modulo NG
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-//Modulo para componentes
-
